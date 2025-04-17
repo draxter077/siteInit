b2812db219a493a56e72608dc17a0a58413279ed
philippe <philippe.idalgoprestes@gmail.com>
2025-04-17 20:55:17 -0300

    all in one

--- main.js
+++ main.js
@@ -138,4 +138,53 @@ window.construct = function construct(p){
 
 //axios.defaults.headers.common["ngrok-skip-browser-warning"] = "69420"
 document.getElementsByTagName("head")[0].appendChild(document.createElement("style"))
+document.getElementsByTagName("style")[0].innerHTML += `
+    html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, 
+    big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center,
+    dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, 
+    aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary,
+    time, mark, audio, video, button, input, textarea {display: block;background: none;margin: 0px;padding: 0px;border: none;
+    font-size: 100%;font: inherit;vertical-align: baseline;text-decoration: none;font-weight: none;outline: none;
+    -webkit-tap-highlight-color: transparent;box-sizing: border-box;}
+    span{display:inline-block;}
+    input:focus, button:focus{outline: none;border: none;}
+    button{cursor: pointer;}
+    body {line-height: 1.3;}
+    ol, ul {list-style: none;}
+    blockquote, q {quotes: none;}
+    blockquote:before, blockquote:after, q:before, q:after {content: '';content: none;}
+    table {border-collapse: collapse;border-spacing: 0;}
+
+    :root{
+    	--colorWhite: rgb(255, 255, 255);
+    	--colorBlack: rgb(0, 0, 0);
+    	--transitionTime: 0.5s;
+    	--animationDelay0: 0s;
+    }
+    body{
+    	box-sizing: border-box;
+    	font-family: system-ui;
+    	color: var(--colorBlack);
+    	cursor: default;
+    }
+    ::-webkit-scrollbar{
+    	height: 3px;
+        width: 3px;
+    	background: none;
+    }
+    ::-webkit-scrollbar-track{
+        background: none;
+    	margin: 10px 0px;
+    }
+    ::-webkit-scrollbar-thumb{
+        background: var(--colorBlack);
+        border-radius: 20px;
+    }
+    ::-webkit-scrollbar-button{
+        height: 10px;
+    	background: none;
+        padding: 0px;
+        margin: 0px;
+    }`
+    
 construct()
